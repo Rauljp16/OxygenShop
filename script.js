7654ffbdb324416a87e7ff891b0e82cc0a56f999
Rauljp16 <raul_astra16@hotmail.com>
2024-04-28 00:42:37 +0200

    crear archivo JS

---
+++ script.js
@@ -0,0 +1,7 @@
+//variables
+const header__menu = document.querySelector(".header__menu")
+const header__menu__menuDesktop = document.querySelector(".header__menu__menuDesktop")
+
+header__menu.addEventListener("click", () => {
+    header__menu__menuDesktop.style.display = "inline-block"
+})
