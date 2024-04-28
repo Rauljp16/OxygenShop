06f7124d72a8afda08828544e83c5eba2817f34c
Rauljp16 <raul_astra16@hotmail.com>
2024-04-28 20:51:47 +0200

    comienzo de menu down

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 //variables
-const header__menu = document.querySelector(".header__menu")
-const header__menu__menuDesktop = document.querySelector(".header__menu__menuDesktop")
+const nav__menu = document.querySelector(".nav__menu");
+const nav__menu__menuDesktop = document.querySelector(
+  ".nav__menu__menuDesktop"
+);
+const nav__svg = document.querySelector(".nav__svg");
+const nav__logo = document.querySelector(".nav__logo");
 
-header__menu.addEventListener("click", () => {
-    header__menu__menuDesktop.style.display = "inline-block"
-})
-// crear section desplegable.
-//prueba commit
+nav__svg.addEventListener("click", () => {
+  nav__menu__menuDesktop.classList.toggle("menuDown");
+  nav__logo.style.display = "block";
+});
+
+//modificar la funcion anterior y seguir con los estilos para lograr
+//que el down se quede 100%
