ce0a024e39f6e70dce21b76856164f19d9b1f74f
Rauljp16 <raul_astra16@hotmail.com>
2024-05-01 12:29:45 +0200

    feat: barra porcentaje de scroll terminada

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const nav__logo = document.querySelector(".nav__logo");
 const toggleMenu = () => {
   menu.classList.remove("menuDown");
   nav__svg.src = "./svg/Menu.svg";
+  nav__svg.style.transform = "rotate(" + -180 + "deg)";
 };
 
 nav__svg.addEventListener("click", () => {
@@ -17,4 +18,17 @@ nav__svg.addEventListener("click", () => {
     nav__svg.src = "./svg/Menu.svg";
     nav__svg.style.transform = "rotate(" + -180 + "deg)";
   }
+});
+//barra porcentaje de scroll
+
+const percentage = document.getElementById("percentage__scroller");
+
+window.addEventListener("load", () => {
+  const update = () => {
+    percentage.style.width = `${
+      (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100
+    }%`;
+  };
+  update();
+  window.addEventListener("scroll", update);
 });
