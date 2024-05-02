b315d5eabacc327cec5f00c206940cb04e23f472
Rauljp16 <raul_astra16@hotmail.com>
2024-05-02 13:07:09 +0200

    feat: Envio de formulario con fetch

--- script.js
+++ script.js
@@ -57,34 +57,78 @@ const delay = () => {
 
 //validate form
 
-const inputName = document.querySelector("#inputName")
-const inputMail = document.querySelector("#inputMail")
-const send = document.querySelector("#send")
-const regexMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-const sendForm = () => {
-  console.log(`nombre: ${inputName.value} mail:${inputMail.value}`)
-  if (regexMail.test(inputMail.value)) {
-    alert("¡El correo electrónico es válido!");
-  } else {
-    alert("¡El correo electrónico no es válido!");
-  }
-  if (inputName.value.length >= 2 && inputName.value.length <= 100) {
-    alert("La longitud  válida.");
-  } else {
-    alert("El contenido debe tener entre 2 y 100 caracteres.");
-  }
-}
-
-
+const inputName = document.querySelector("#inputName");
+const inputMail = document.querySelector("#inputMail");
+const sendForm = document.querySelector("#sendForm");
+const regexMail =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const checkbox = document.querySelector("#checkbox");
+let formValid = false;
 
-checkbox.addEventListener('click', function () {
-  checkbox.classList.toggle("checkboxCheckImg")
-  if (checkbox.checked) {
-    console.log('El checkbox está marcado.');
+const validations = () => {
+  if (regexMail.test(inputMail.value)) {
+    inputMail.classList.remove("error");
   } else {
-    console.log('El checkbox no está marcado.');
+    inputMail.classList.add("error");
   }
 
+  if (inputName.value.length >= 2 && inputName.value.length <= 100) {
+    inputName.classList.remove("error");
+  } else {
+    inputName.classList.add("error");
+  }
+
+  if (checkbox.checked) {
+    checkbox.classList.remove("error");
+  } else {
+    checkbox.classList.add("error");
+  }
+
+  if (
+    regexMail.test(inputMail.value) &&
+    inputName.value.length >= 2 &&
+    inputName.value.length <= 100 &&
+    checkbox.checked
+  ) {
+    formValid = true;
+  }
+};
+
+checkbox.addEventListener("click", function () {
+  checkbox.classList.toggle("checkboxCheckImg");
 });
+
+const resetForm = () => {
+  inputName.value = "";
+  inputMail.value = "";
+  checkbox.checked = false;
+  checkbox.classList.remove("checkboxCheckImg");
+  formValid = false;
+};
+
+// Mandar datos del for a API de testing
+
+sendForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  validations();
+  if (formValid === true) {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        name: inputName.value,
+        email: inputMail.value,
+        consent: checkbox.checked,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+    resetForm();
+  } else {
+    alert("Formulario no valido");
+  }
+});
+
+//popup / modal
