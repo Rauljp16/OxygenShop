8c9b3e4d87ddd4a014a542fafb1f1e12ed4f7dab
Rauljp16 <raul_astra16@hotmail.com>
2024-05-12 12:50:41 +0200

    refactor:cambio de el error del formulario a modal

--- slider.js
+++ slider.js
@@ -14,32 +14,39 @@ class slider {
       "./img/shanghai.webp",
     ];
   }
+
   windowListener() {
     this.sliderImg.src = this.imgArray[0];
     this.interval = setInterval(() => this.nextPage(), 12000);
     this.buttonRight.addEventListener("click", () => this.nextPage());
     this.buttonLeft.addEventListener("click", () => this.previousPage());
+
+    // Agregar listeners para los paginadores
+    this.span.forEach((paginator, index) => {
+      paginator.addEventListener("click", () => this.goToPage(index));
+    });
   }
+
   nextPage() {
     this.contador++;
     if (this.contador > this.imgArray.length - 1) {
       this.contador = 0;
     }
     this.sliderImg.src = this.imgArray[this.contador];
-    this.span[this.contador].classList.add("paginations--active");
-    this.paginationsNext();
+    this.updatePaginationClasses();
     this.restartInterval();
   }
+
   previousPage() {
     this.contador--;
     if (this.contador < 0) {
       this.contador = this.imgArray.length - 1;
     }
     this.sliderImg.src = this.imgArray[this.contador];
-    this.span[this.contador].classList.add("paginations--active");
-    this.paginationsPrevious();
+    this.updatePaginationClasses();
     this.restartInterval();
   }
+
   paginationsNext() {
     if (this.contador === 0) {
       this.span[this.imgArray.length - 1].classList.remove(
@@ -49,6 +56,7 @@ class slider {
       this.span[this.contador - 1].classList.remove("paginations--active");
     }
   }
+
   paginationsPrevious() {
     if (this.contador === this.imgArray.length - 1) {
       this.span[0].classList.remove("paginations--active");
@@ -56,13 +64,33 @@ class slider {
       this.span[this.contador + 1].classList.remove("paginations--active");
     }
   }
+
+  goToPage(index) {
+    this.contador = index;
+    this.sliderImg.src = this.imgArray[this.contador];
+    this.updatePaginationClasses();
+    this.restartInterval();
+  }
+
+  updatePaginationClasses() {
+    this.span.forEach((paginator, index) => {
+      if (index === this.contador) {
+        paginator.classList.add("paginations--active");
+      } else {
+        paginator.classList.remove("paginations--active");
+      }
+    });
+  }
+
   restartInterval() {
     clearInterval(this.interval);
     this.interval = setInterval(() => this.nextPage(), 12000);
   }
+
   start() {
     this.windowListener();
   }
 }
+
 const mySlider = new slider();
 mySlider.start();
